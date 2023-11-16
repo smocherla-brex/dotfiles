15e4bc6c9220bb2d17e8a9bbbf1e853d658cb8f2
Sridhar Mocherla <smocherla@brex.com>
2023-11-15 20:11:43 -0500

    Comment out this?

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ scripts/intellij.sh
 
 echo "Importing IDE settings..."
 mkdir -p ~/.config/JetBrains/RemoteDev-IU/_home_owner_src_credit_card/
-unzip settings.zip -d ~/.config/JetBrains/RemoteDev-IU/_home_owner_src_credit_card/
+# unzip settings.zip -d ~/.config/JetBrains/RemoteDev-IU/_home_owner_src_credit_card/
 
